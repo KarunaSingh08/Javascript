8b357cabe0552e55f44eb2fdd25c5c851872eda7
KarunaSingh08 <131587289+KarunaSingh08@users.noreply.github.com>
2023-12-23 07:23:15 +0000

    math

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -16,4 +16,19 @@ const hundreds = 1000000;
 
 // +++++++++++++++ Maths ++++++++++++++++++++++++++++++
 
-console.log(Math);
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 8, 6));
+// console.log(Math.max(4, 3, 8, 6));
+
+console.log(Math.random());
+console.log(Math.random() * 10 + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
