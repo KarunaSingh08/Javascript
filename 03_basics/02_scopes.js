9fe2802a7a513960648876bf46de68c1ea717607
KarunaSingh08 <131587289+KarunaSingh08@users.noreply.github.com>
2024-01-03 08:52:52 +0000

    scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,6 +6,30 @@ if (true) {
   console.log("INNER: ", a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one() {
+  const username = "karuna";
+
+  function two() {
+    const website = "youtube";
+    console.log(username);
+  }
+  // console.log(website);
+
+  two();
+}
+
+// one();
+
+if (true) {
+  const username = "karuna";
+  if (username === "karuna") {
+    const website = " youtube";
+    console.log(username + website);
+  }
+  console.log(website);
+}
+console.log(username);
