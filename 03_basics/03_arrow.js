e3e8477d1bf26df648f81a328e03424d71b87ac7
KarunaSingh08 <131587289+KarunaSingh08@users.noreply.github.com>
2024-01-05 03:29:06 +0000

    arrow

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -1,11 +1,52 @@
 const user = {
   username: "karuna",
-  price: 999
+  price: 999,
 
-  welcomeMessage: function(){
-    console.log(`${this.username}, welcome to website`)
-  }
+  welcomeMessage: function () {
+    console.log(`${this.username}, welcome to website`);
+    console.log(this);
+  },
+};
 
-}
+// user.welcomeMessage();
+// user.username = "sam";
+// user.welcomeMessage();
 
-user.welcomeMessage
+// console.log(this);
+
+// function chai() {
+//   let username = "karuna";
+//   console.log(this.username);
+// }
+
+// chai();
+
+// const chai = function () {
+//   let username = "karuna";
+//   console.log(this.username);
+// };
+
+// arrow function
+
+const chai = () => {
+  let username = "karuna";
+  console.log(this);
+};
+
+// chai();
+
+// const addTwo = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// const addTwo = (num1, num2) => num1 + num2;
+
+// const addTwo = (num1, num2) => num1 + num2;
+
+const addTwo = (num1, num2) => ({ username: "karuna" });
+
+console.log(addTwo(3, 4));
+
+// const myArray = [2, 5, 3, 7, 8];
+
+// myArray.forEach();
